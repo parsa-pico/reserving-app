ca2c642782d22d5431f7a3746b4a16ca537181ab
Parsa Foroozmand <parsaforoozmand@gmail.com>
2022-09-15 21:57:01 +0430

    #

--- src/Components/ReserveBox.jsx
+++ src/Components/ReserveBox.jsx
@@ -21,7 +21,7 @@ export default function ReserveBox() {
     }
     const credentials = Realm.Credentials.apiKey(process.env.REACT_APP_API_KEY);
     app.logIn(credentials);
-    console.log(app.currentUser);
+    console.log(process.env);
   };
   useEffect(() => {
     getInfo();
