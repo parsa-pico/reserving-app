afd1ed9ab02fadc58bec715eea92e2a7ac1fbeb7
Parsa Foroozmand <parsaforoozmand@gmail.com>
2022-09-17 20:53:52 +0430

    #

--- src/Components/realmConfig.js
+++ src/Components/realmConfig.js
@@ -1,4 +1,7 @@
 import * as Realm from "realm-web";
+// export const app = new Realm.App({
+//   id: process.env.REACT_APP_MONGODB_SERVICE_KEY,
+// });
 export const app = new Realm.App({
-  id: process.env.REACT_APP_MONGODB_SERVICE_KEY,
+  id: "mongodbapp-mctcp",
 });
