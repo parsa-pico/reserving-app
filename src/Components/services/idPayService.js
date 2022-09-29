3c3297b1257d0a7a448d6472bc0ae9ba9ba407d5
Parsa Foroozmand <parsaforoozmand@gmail.com>
2022-09-29 16:06:32 +0330

    #

--- src/Components/services/idPayService.js
+++ src/Components/services/idPayService.js
@@ -9,7 +9,7 @@ async function payment(name) {
   const { id: paymentId, link: paymentLink } = result.data;
   return { paymentId, paymentLink };
 }
-async function verify(id, order_id) {
+async function verify(id, order_id, track_id) {
   const result = await app.currentUser.functions.idPayValidation(
     id,
     order_id,
